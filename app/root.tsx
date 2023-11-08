a57f1af8d9770e7eeb41a5a46a1be39497bfc56a
Mona Lisa <joorysalam786@gmail.com>
2023-11-08 20:49:52 +0530

    added unocss

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,6 @@
+import type { LinksFunction } from "@vercel/remix";
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
@@ -6,6 +8,12 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { ReactNode } from "react";
+import "virtual:uno.css";
+import "@unocss/reset/tailwind.css";
+import styles from "./app.css?inline";
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export default function App() {
   return (
@@ -17,11 +25,25 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <LiveReload />
-        <Scripts />
+        <Layout>
+          <Outlet />
+          <ScrollRestoration />
+          <LiveReload />
+          <Scripts />
+        </Layout>
       </body>
     </html>
+  );
+}
+
+function Layout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <nav className="px-10 pt-5"></nav>
+      <Link to="/" prefetch="intent" className="text-2xl font-semibold">
+        Move<span className="text-teal-500">DB</span>
+      </Link>
+      <main>{children}</main>
+    </>
   );
 }
